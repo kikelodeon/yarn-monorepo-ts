57bf427fc04491811683b2369451a176874a31e9
Quique <quiquesg88@gmail.com>
2025-01-26 14:26:48 +0100

    save

--- packages/application/user/src/errors/EmailAlreadyInUseError.ts
+++ packages/application/user/src/errors/EmailAlreadyInUseError.ts
@@ -4,11 +4,10 @@ import { ConflictError } from '@kikerepo/contracts-common'; // Import the Valida
 export class EmailAlreadyInUseError extends ConflictError {
   constructor(email: string) {
     // Create a detailed validation error for the email
-    const validationErrors = {
-      email: [`The email ${email} is already in use`],
-    };
+    super();
+   this.message = 'Email is already in use.';
 
     // Call the parent constructor to set the message and validation errors
-    super(validationErrors, { email });
+
   }
 }
